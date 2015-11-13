929b0f23ffa34c4faca83e2fe38ef7811b6f8626
terletskyi23 <terletskyi23@gmail.com>
2015-11-13 18:31:06 +0200

    Terletskyi: Add authentication using gem 'devise'

--- MarkdownBlog/config/routes.rb
+++ MarkdownBlog/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'persons/profile', as: 'user_root'
+  root 'posts#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
