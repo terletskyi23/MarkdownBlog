1798c1cb14f403af4a23b621cbc983435bedfe54
terletskyi23 <terletskyi23@gmail.com>
2015-11-15 17:58:51 +0200

    VST: Create rails app with postgresql

--- MarkdownBlog/config/routes.rb
+++ MarkdownBlog/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'persons/profile', as: 'user_root'
-  root 'posts#home'
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
